420c75c57ad1cddd64d2b2169b67272baf71244e
tesilio <lodcoco@gmail.com>
2023-11-17 01:35:10 +0900

    feat: 웹훅 이벤트들 추가

--- src/handlers/webHook.ts
+++ src/handlers/webHook.ts
@@ -1,13 +1,22 @@
 import { TelegramBot } from '../TelegramBot';
 import * as http from 'serverless-http';
 import { message } from 'telegraf/filters';
+import {
+  Telegraf
+} from 'telegraf';
 
 const telegraf = TelegramBot.getTelegraf();
 
+// info: on 이벤트가 맨 밑으로 가야 할 듯
+telegraf.help((ctx) => ctx.reply('Send me a sticker'))
+telegraf.command('oldschool', (ctx) => ctx.reply('Hello'));
+telegraf.command('hipster', Telegraf.reply('λ');
+telegraf.on(message('sticker'), (ctx) => ctx.reply('👍'))
 telegraf.on(message('text'), async (ctx) => {
   const text = ctx.message.text;
   await ctx.reply(text);
 });
+
 
 /**
  * 핸들러
