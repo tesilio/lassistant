8be27e975ed03eb5a8b177e79163abd3cbe9a968
tesilio <lodcoco@gmail.com>
2023-11-17 01:08:48 +0900

    fix: 웹훅 핸들러 경로 수정

--- src/handlers/webHook.ts
+++ src/handlers/webHook.ts
@@ -1,16 +1,13 @@
-import {
-  TelegramBot,
-} from '../TelegramBot';
+import { TelegramBot } from '../TelegramBot';
 import * as http from 'serverless-http';
 import { message } from 'telegraf/filters';
+
 const telegraf = TelegramBot.getTelegraf();
 
-telegraf.on(message('text'), async (ctx) => {
-  await ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-})
+telegraf.on(message('text'), (ctx) => ctx.reply('Hello'));
 
 /**
  * 핸들러
  * @type {ServerlessHttp.Handler}
  */
-export const handler = http(telegraf.webhookCallback('/telegraf'));
+export const handler = http(telegraf.webhookCallback());
