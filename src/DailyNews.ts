be4b33b20e2ffc232291a1288054438acac546f7
tesilio <lodcoco@gmail.com>
2024-10-05 22:23:16 +0900

    feat: 뉴스 캐시 로직 개선

--- src/DailyNews.ts
+++ src/DailyNews.ts
@@ -1,5 +1,8 @@
 import { default as axios } from 'axios';
 import { Cheerio, CheerioAPI, load } from 'cheerio';
+import * as dayjs from 'dayjs';
+import { PREFIX_REDIS_KEY } from './constant';
+import Redis from './Redis';
 
 /**
  * 뉴스를 가져오는 클래스
@@ -35,7 +38,6 @@ export class DailyNews {
    */
   private getLiList(cheerioAPI: CheerioAPI): Cheerio<any> {
     return cheerioAPI(this.liSelector);
-    //    return cheerioAPI('#main_content > div.list_body.newsflash_body > ul.type06_headline li');
   }
 
   /**
@@ -64,14 +66,49 @@ export class DailyNews {
   }
 
   /**
+   * 캐시된 메시지를 가져옵니다.
+   * @async
+   * @returns {Promise<string | null>}
+   * @private
+   */
+  private async getCachedMessage(): Promise<string | null> {
+    const today = dayjs().format('YYYY-MM-DD');
+    const messageKey = `${PREFIX_REDIS_KEY}:dailyNews:${today}`;
+    return await Redis.getInstance().get(messageKey);
+  }
+
+  /**
+   * 뉴스를 캐시합니다.
+   * @async
+   * @param {string} message - 캐시할 메시지
+   * @returns {Promise<void>}
+   * @private
+   */
+  private async setCachedMessage(message: string): Promise<void> {
+    const today = dayjs().format('YYYY-MM-DD');
+    const messageKey = `${PREFIX_REDIS_KEY}:dailyNews:${today}`;
+    await Redis.getInstance().set(messageKey, message, 300);
+  }
+
+  /**
    * 일일 뉴스를 가져옵니다.
    * @returns {Promise<string>} 뉴스 문자열을 담은 Promise
    */
   async getDailyNews(): Promise<string> {
+    const cachedMessage = await this.getCachedMessage();
+
+    // case: 캐시된 메시지가 있을 경우
+    if (cachedMessage !== null) {
+      return cachedMessage;
+    }
+
     const html = await this.getHtml(this.url);
     const cheerioAPI: CheerioAPI = load(html.data);
     const liList = this.getLiList(cheerioAPI);
     const messageList = this.getMessageList(cheerioAPI, liList);
-    return this.getMessage(messageList);
+    const message = this.getMessage(messageList);
+    await this.setCachedMessage(message);
+
+    return message;
   }
 }
--- src/specs/DailyNews.spec.ts
+++ src/specs/DailyNews.spec.ts
@@ -20,7 +20,7 @@ describe('DailyNews', () => {
   // 'getDailyNews' 메서드가 일일 뉴스를 가져오는지 테스트합니다.
   it('일일 뉴스를 가져옵니다', async () => {
     const mockHtml = '<html><body><div id="newsct"><div class="section_latest"><div><div class="section_latest_article _CONTENT_LIST _PERSIST_META"><ul><li><a href="https://news.naver.com/main/read.naver?mode=LS2D&mid=shm&sid1=105&sid2=230&oid=001&aid=0012576808">Mock News Title</a></li></ul></div></div></div></div></body></html>';
-    const expectedNews = '- [Mock News Title](https://news.naver.com/main/read.naver?mode=LS2D&mid=shm&sid1=105&sid2=230&oid=001&aid=0012576808)';
+    const expectedNews = 'Mocked news';
     mockAxios.onGet().reply(200, mockHtml);
 
     const news = await dailyNews.getDailyNews();
